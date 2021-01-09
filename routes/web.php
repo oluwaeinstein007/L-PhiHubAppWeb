57cbd7081d35a7dc380b796f41ea6a50196fda6c
Sanni, Olanrewaju <slanre26@gmail.com>
2021-01-09 19:11:31 +0100

    Gmail verification done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,13 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('facebook')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('facebook')->user();
+
+    // $user->token
+});
